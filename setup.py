65944f418f27c52a78218f516484b1b2112e97f3
qiaopeng <john_04047210@163.com>
2019-06-21 19:02:34 +0900

    modify setup.py add extra info

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from distutils.core import setup, Extension
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='pyhuffman', 
     version='1.0', 
-    ext_modules=[Extension('pyhuffman', ['huffman.c','huffmanWrapper.c'])]
+    author='QiaoPeng',
+    author_email='john_04047210@163.com',
+    description='compress or decompress with huffman',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/john04047210/pyhuffman.git',
+    ext_modules=[Extension('pyhuffman', ['huffman.c','huffmanWrapper.c'])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
